091f456a49b8b831e464cc32bcf905e26070de44
Dariusz Pilecki <pileckidariusz90@gmail.com>
2022-10-05 02:35:50 +0200

    Responsivity hot fixes

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -36,7 +36,7 @@ const About = () => {
             <article ref={about}>
                 <h2>O mnie <strong>czyli</strong>...</h2>
                 <p>Inżynier z powołania. Uwielbiam gdy coś działa, ale najwięcej satysfakcji mam gdy działa po dobrej
-                    przeprawie z wszystkimi co działać nie chce. Zasada jest prosta: nie odpuszczać aż się uda. Nie lubię przerostu teori
+                    przeprawie z wszystkimi co działać nie chce. Zasada jest prosta: nie odpuszczać aż się uda. Nie lubię przerostu teorii
                     nad działaniami (czyli dużo gadać, mało robić), chyba dlatego swoją przygodę z uczelnią zakończyłem na etapie tytułu inżyniera. Głowę muszę mieć cały
                     czas czymś zajętą, stąd cierpię na chroniczny brak czasu. Jeśli w coś wchodzę to na całego. Wymyślanie koła od nowa to moje hobby,
                     ale zazwyczaj mam na nie czas dopiero po godzinach pracy. W robocie lubię na skróty. Lubię wiedzieć, jak nie wiem to pytam, dlatego fajnie
@@ -51,10 +51,10 @@ const About = () => {
                     zajmuję się instalacjami opalania, chłodzenia, pneumatyką, oraz rozruchami (czyli też rozgrzewami) pieców szklarskich.
                     Dużo hałasu, ciężkiego sprzętu, ludzi, procesów, instalacji... to właśnie moje środowisko. Ale radość z pierwszego szkła... Kto
                     nie przeżył, nie zrozumie. Swoje życie zawodowe realizuję na całym świecie. Pracujemy od Indii przez Arabię Saudyjską, Egipt, Turcję
-                    aż po kraje zachodniej europy. Mogę pochwalić się współpracą z takimi potęgami jak Saint Gobain, Owens Ilinois, Philips Lightening (obecnie Signify)
+                    aż po kraje zachodniej Europy. Mogę pochwalić się współpracą z takimi potęgami jak Saint Gobain, Owens Illinois, Philips Lightening (obecnie Signify)
                     Stoelzle, czy Isover.
                     Jako typowy "Gaziarz" posiadam szereg uprawnień ( Ciepłownicze, Gazowe, Elektryczne). W ramach rozrywki w pracy czasem pośmigam na wózku widłowym
-                    (przypadkiem zrobiłem uprawnienia UDT). Prace ślusarskie i spawalcznicze też lubię i też potrafię. W wyniku tejże pracy zacząłęm się też interesować
+                    (przypadkiem zrobiłem uprawnienia UDT). Prace ślusarskie i spawalnicze też lubię i też potrafię. W wyniku tejże pracy zacząłem się też interesować
                     automatyką. Generalnie nie boję się żadnego rodzaju pracy...
                 </p>
             </article>
@@ -62,8 +62,8 @@ const About = () => {
                 <h2>Gdzie zmierzam...</h2>
                 <p>
                     No właśnie, więc po co to całe zamieszanie ? Od instalacji przez automatykę... no wiadomo. Programowanie. Od ponad dwóch
-                    lat zajmuję się (oraz bardzo interesuję się) programowaniem. Do dzisija nie wiem dlaczego padło na Javascript.
-                    Ale to na nim się głównie skupiłem. Chociaż proste programowanie mikrokontrolerów w C++ też nie jest mi obce. No i jescze Raspberry.
+                    lat zajmuję się (oraz bardzo interesuję się) programowaniem. Do dzisiaj nie wiem dlaczego padło na Javascript.
+                    Ale to na nim się głównie skupiłem. Chociaż proste programowanie mikrokontrolerów w C++ też nie jest mi obce. No i jeszcze Raspberry.
                     A jak Raspberry to oczywiście Python. Nie może być inaczej. Generalnie mam za sobą kilkanaście projektów w różnych technologiach
                     głownie JS`owych. Kilka zrobiłem komercyjnie. Niestety brak mi doświadczenia w pracy zespołowej, przy dużych projektach, z dużymi
                     możliwościami rozwoju. I tu jest właśnie mój cel. Tu właśnie zmierzam. Chcę zawodowo programować.
@@ -72,11 +72,11 @@ const About = () => {
             <article ref={programming}>
                 <h2>Więc co z tym <strong>programowaniem</strong> ?</h2>
                 <p>
-                    Po prostu lubię to. Lubię opanować nowy temat i od razu gdzieś go zastosować. Może dlatego korzystam z technologi zarówno
+                    Po prostu lubię to. Lubię opanować nowy temat i od razu gdzieś go zastosować. Może dlatego korzystam z technologii zarówno
                     frontendowych jak i backendowych. Bardzo dobrze czuję się też z TypeScript. Jeden z lepszych wynalazków dla JS moim zdaniem.
-                    A więc, radzę sobie nieźle z node: w grę wchodzi oczywiśćie Express i coś co ostanio spędza mi sen z powiek czyli Nest.
-                    Nieodzowną częscią backendu są bazy danych. Jestem fanem baz mySql, ale z mongoDB też daję radę. Potrafię napisać serwer websocket. Uwierzytelnianie
-                    i autoryzacje i logowania ograniam, chociaż tematy bezpieczeństwa to temat rzeka który staram się zgłebiać kiedy tylko jest taka możliwość.
+                    A więc, radzę sobie nieźle z node: w grę wchodzi oczywiście Express i coś co ostatnio spędza mi sen z powiek czyli Nest.
+                    Nieodzowną częścią backendu są bazy danych. Jestem fanem baz mySql, ale z mongoDB też daję radę. Potrafię napisać serwer websocket. Uwierzytelnianie
+                    i autoryzacje i logowania ogarniam, chociaż tematy bezpieczeństwa to temat rzeka który staram się zgłębiać kiedy tylko jest taka możliwość.
                     Jeśli chodzi o frontend, to stawiam na React. Radzę sobie z hookami, Reduxem, Mobexem, Routerami... No nie oszukujmy się,
                     poczucie estetyki mam marne. Ale jeśli ktoś pokaże dokładnie jak coś ma wyglądać to wiem jak korzystać z CSS,SASS, Canvas, SGV,
                     no i magia: Potrafię w miarę skonfigurować Webpacka. GIT i html5 to raczej oczywiste. Mój prawdziwy konik to jak łączy się internet z automatyką... czyli słynne IOT.
